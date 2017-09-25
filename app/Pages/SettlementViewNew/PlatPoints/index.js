0639beefeba601b68f547a39a13d3c50b6a90f63
Ivan <15101126742@163.com>
2017-09-25 11:20:03 +0800

    Update index.js

--- app/Pages/SettlementViewNew/PlatPoints/index.js
+++ app/Pages/SettlementViewNew/PlatPoints/index.js
@@ -8,15 +8,20 @@ import {
 } from 'react-native';
 
 export default class PlatPoints extends Component {
-
+   
+  state = {
+    checked: false
+  }
+  
   render() {
     let { platPoints } = this.props
     console.log(platPoints);
     if (!platPoints) return null
 
     return (
+      let checkURI = this.state.checked ? 'https://static-o2o.360buyimg.com/daojia/new/images/icon/icon_checked.png' : 'https://static-o2o.360buyimg.com/daojia/new/images/icon/icon_checkbox_none.png'
       <View style={styles.container}>
-        <Image source={uri: 'https://static-o2o.360buyimg.com/daojia/new/images/icon/icon_checkbox_none.png'} style={styles.checkImage}></Image>
+        <Image source={uri: checkURI} style={styles.checkImage}></Image>
         <Text>{platPoints.platPointsDesc}</Text>
       </View>
     );
