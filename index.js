43ada460455c00c95a26565ba7b6f6945911ccc6
Ivan <wcy5211678@163.com>
2017-10-31 18:21:47 +0800

    index

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+// import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native'
+import './app/Utils/interceptors.js'
+import DaojiaRN from './app/index.js'
+// import DaojiaRN from './app/components/StoreHome/index.js'
+// import DaojiaRN from './DaojiaReactNative/App.js'
+
+console.log("init")
+
+AppRegistry.registerComponent('DaojiaRN', () => DaojiaRN)
