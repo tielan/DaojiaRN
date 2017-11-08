7ca1d97d1fa9a9c16a3037a954d8015bdd297418
Ivan <wcy5211678@163.com>
2017-11-08 16:45:03 +0800

    bridge

--- host_bridge.js
+++ host_bridge.js
@@ -2,9 +2,14 @@ import Init from './DaojiaReactNative/partner/local/Init';
 import AutoLogin from './DaojiaReactNative/partner/local/AutoLogin';
 import ToLoginPage from './DaojiaReactNative/partner/local/ToLoginPage';
 
+import NavigatorIOSReform from './DaojiaReactNative/common/NavigatorIOSReform';
+
+
+
 
 module.exports = {
     Init:Init,
     AutoLogin:AutoLogin,
-    ToLoginPage:ToLoginPage
+    ToLoginPage:ToLoginPage,
+    NavigatorIOSReform:NavigatorIOSReform
 };
